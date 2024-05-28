2ea1c8437bdd4a2c7a5c3e5e74ae7d19390044b1
Vincent Duarte <vincentdu@theodo.fr>
2024-05-28 22:44:38 +0200

    add routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
 "use client"
+import axios from 'axios';
 import { useEffect, useState } from "react";
 
-
-// export const getServerSideProps = async () => {
-//   const res = await fetch('http://3000/api/hello'); // Remplacez cette URL par celle de votre API
-//   const data = await res.json();
-//   console.log("respons = " + res);
-
-//   return {
-//     props: {
-//       name: data.pseudo, // Assurez-vous que la réponse de votre API contient un champ `firstName`
-//     },
-//   };
-// };
-
 export default function Home() {
-  const [pseudo, setFirstName] = useState<string>('');
+
+  const routeCallAuto = 'https://test-cors-drab.vercel.app/api/hello';
+  const routeGetAlice = 'https://test-cors-drab.vercel.app/api/hello';
+  const routeDeleteBob = 'https://test-cors-drab.vercel.app/api/hello';
+
+
+  const [pseudoGetAlice, setFirstName] = useState<string>('');
 
   useEffect(() => {
     const fetchFirstName = async () => {
-      const res = await fetch('https://test-cors-drab.vercel.app/api/hello');
+      const res = await fetch(routeCallAuto);
       console.log("respons = " + res);
       const data = await res.json();
       setFirstName(data.pseudo);
@@ -27,6 +21,38 @@ export default function Home() {
 
     fetchFirstName();
   }, []);
+
+  const [dataAlice, setDataAlice] = useState<string | null>(null);
+  const [loadingAlice, setLoadingAlice] = useState<boolean>(false);
+  const [errorAlice, setErrorAlice] = useState<string | null>(null);
+  const handleClickAlice = async () => {
+    setLoadingAlice(true);
+    setErrorAlice(null);
+    try {
+      const response = await axios.get(routeGetAlice); // Remplacez '/api/data' par votre URL de l'API
+      setDataAlice(response.data.pseudo);
+    } catch (error) {
+      setErrorAlice('Erreur lors de la récupération des données');
+    } finally {
+      setLoadingAlice(false);
+    }
+  };
+
+  const [dataBob, setDataBob] = useState<string | null>(null);
+  const [loadingBob, setLoadingBob] = useState<boolean>(false);
+  const [errorBob, setErrorBob] = useState<string | null>(null);
+  const handleClickBob = async () => {
+    setLoadingBob(true);
+    setErrorBob(null);
+    try {
+      const response = await axios.delete(routeDeleteBob); // Remplacez '/api/data' par votre URL de l'API
+      setDataBob(response.data.pseudo);
+    } catch (error) {
+      setErrorBob('Erreur lors de la récupération des données');
+    } finally {
+      setLoadingBob(false);
+    }
+  };
   
   return (
     <main>
@@ -34,13 +60,25 @@ export default function Home() {
       <h1 style={{ color: 'green', textAlign: 'center' }}>Mon site</h1>
 
       <div style={{textAlign: 'center'}}>
-          <p> Je suis : {pseudo}</p>
+          <p> Call auto : {pseudoGetAlice}</p>
       </div>
 
       <div style={{textAlign: 'center'}}>
-        <h4>Nouveau message :</h4>
-        <p> Message de Charlie : <a href="http://localhost:6660/">Va voir cet autre site sur les CORS !! Aller clique il est top !!!</a></p>
+      <h3>Alice :</h3>
+      <button onClick={handleClickAlice}>GET</button>
+      {loadingAlice && <p>Chargement...</p>}
+      {errorAlice && <p>{errorAlice}</p>}
+      {dataAlice && <p>Données: {dataAlice}</p>}
       </div>
+
+      <div style={{textAlign: 'center'}}>
+      <h3>Bob :</h3>
+      <button onClick={handleClickBob}>DELETE</button>
+      {loadingBob&& <p>Chargement...</p>}
+      {errorBob && <p>{errorBob}</p>}
+      {dataBob && <p>Données: {dataBob}</p>}
+      </div>
+
     </main>
   );
 }
