11e748edf5b77bbbf03caa475ab2c3dedab738f0
Vincent Duarte <vincentdu@theodo.fr>
2024-05-29 09:19:58 +0200

    update cors

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   const routeCallAuto = 'https://test-cors-drab.vercel.app/api/hello';
   const routeGetAlice = 'https://test-cors-drab.vercel.app/api/hello';
-  const routeDeleteBob = 'https://test-cors-second.vercel.app';
+  const routeDeleteBob = 'https://test-cors-drab.vercel.app/api/hello';
 
 
   const [pseudoGetAlice, setFirstName] = useState<string>('');
