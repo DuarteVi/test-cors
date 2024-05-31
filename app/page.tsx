baf7657f7bd1a8dff4229e1a78a00788dd140908
Vincent Duarte <vincentdu@theodo.fr>
2024-05-31 09:40:19 +0200

    feat(vincent): update cors

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
   const routeCallAuto = 'https://test-cors-drab.vercel.app/api/hello';
   const routeGetAlice = 'https://test-cors-second.vercel.app/api/hello';
   const headerGetALice = {
+    Cookie: 'MonSuperCookie' , 
     withCredentials: true
   };
   const routeDeleteBob = 'https://test-cors-second.vercel.app/api/hello';
