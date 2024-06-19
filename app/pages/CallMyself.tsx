2242dd879cccde23a2671202bba508f73a30125b
Vincent Duarte <vincentdu@theodo.fr>
2024-06-19 18:08:54 +0200

    feat: some progress

---
+++ app/pages/CallMyself.tsx
@@ -0,0 +1,14 @@
+
+const CallMyself = () => {
+
+    const route = 'http://my-site:3000/data';
+
+  return (
+    <div>
+      <h2>Do request on the same origin.</h2>
+
+    </div>
+  );
+};
+
+export default CallMyself;
