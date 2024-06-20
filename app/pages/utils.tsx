b2c4d979955c1295f0253b27a6e75918261e83f9
Vincent Duarte <vincentdu@theodo.fr>
2024-06-20 09:33:53 +0200

    feat: some progress 6

--- app/pages/utils.tsx
+++ app/pages/utils.tsx
@@ -1,13 +1,14 @@
+
+export interface CallSection {
+  url: string;
+}
+
 export const HttpMethod = {
     GET: 'GET',
     DELETE: 'DELETE',
   };
 
-  export const URL = {
-    SAME_ORIGIN: 'https://test-cors-drab.vercel.app',
-    REMOTE_VALID_ORIGIN: 'https://test-cors-second.vercel.app',
-  };
-
-  export interface CallSection {
-    url: string;
-  }
+export const URL = {
+  SAME_ORIGIN: 'https://test-cors-drab.vercel.app',
+  REMOTE_VALID_ORIGIN: 'https://test-cors-second.vercel.app',
+};
