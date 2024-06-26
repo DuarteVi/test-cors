054752ca151e6ba4bf8c9e462b32dcf2419b45c3
Vincent Duarte <vincentdu@theodo.fr>
2024-06-26 13:53:33 +0200

    feat: some progress 10

--- app/pages/CallRemoteNobodyOrigin.tsx
+++ app/pages/CallRemoteNobodyOrigin.tsx
@@ -7,7 +7,7 @@ const CallRemoteNobodyOrigin: React.FC<CallSection> = ({index, url}) => {
   return (
     <div>
       <h3>{index}. allows only &quot;other-website.com&quot;</h3>
-      <p>In this section, my frontend call a server on the remote origin. The server define CORS to accept only "other-website.com" without any method.</p>
+      <p>In this section, my frontend call a server on the remote origin. The server define CORS to accept only &quot;other-website.com&quot; without any method.</p>
       <p>Take a look how the DELETE request is not even sent.</p>
 
       <Code source={URL.REMOTE_NOBODY_ORIGIN} origin={"other-website.com"}></Code>
--- app/pages/CallRemotePutMethod.tsx
+++ app/pages/CallRemotePutMethod.tsx
@@ -7,7 +7,7 @@ const CallRemotePutMethod: React.FC<CallSection> = ({index, url}) => {
   return (
     <div>
       <h3>{index}. allows only method &quot;PUT&quot;</h3>
-      <p>In this section, my frontend call a server on the remote origin. The server define CORS to accept any website but with only the "PUT" method.</p>
+      <p>In this section, my frontend call a server on the remote origin. The server define CORS to accept any website but with only the &quot;PUT&quot; method.</p>
 
       <Code source={URL.REMOTE_PUT_METHOD} origin={"*"} methods={"PUT"}></Code>
       <MakeACall method={HttpMethod.GET} url={url}></MakeACall>
